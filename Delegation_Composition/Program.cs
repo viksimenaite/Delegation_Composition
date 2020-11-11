8530d6d1cc963f5f2c98b12d008ae53f9ecabc08
viksimenaite <55063269+viksimenaite@users.noreply.github.com>
2020-11-11 22:25:42 +0200

    Add project files.

---
+++ Delegation_Composition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Delegation_Composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Delegation_Composition/Suitability.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegation_Composition
+{
+    interface Suitability
+    {
+    }
+}
